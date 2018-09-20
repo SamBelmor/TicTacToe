44001a969e50a5d85a86dbcef30b6703064572bd
Dzoara Bello <sambelmor@gmail.com>
2018-09-20 16:27:26 -0500

    TicTacToe game

---
+++ tic_tac_toe.rb
@@ -0,0 +1,67 @@
+# load 'player.rb'
+# load 'board.rb'
+require_relative 'player'
+require_relative 'board'
+
+class TicTacToe
+  attr_accessor :board, :current_player
+  
+  def initialize
+    @board = Board.new
+    @player_one = Player.new("Player one", "X")
+    @player_two = Player.new("Player two", "O")
+    @current_player = @player_one
+  end
+
+  def play
+    while !check_if_game_over
+      puts "#{current_player.name} is your turn"
+      board.render_board
+      coordinates = current_player.ask_for_coordinates
+      if board.could_draw?(coordinates)
+        board.draw_symbol(coordinates, current_player.piece)
+        unless check_if_game_over
+          switch_players
+        else
+          board.render_board
+        end
+      else
+        puts "That position it's ocupied"
+      end
+    end
+  end
+
+  def check_if_game_over
+    check_victory || check_draw
+  end
+  
+  def check_victory
+    if @board.winning_combination?(@current_player.piece)
+      puts "#{@current_player.name} wins"
+      true
+    else
+      false
+    end
+  end 
+
+  def check_draw
+    if @board.full
+      puts "It's a draw"
+      true
+    else
+      false
+    end
+  end
+  
+  def switch_players
+    if @current_player == @player_one
+      @current_player = @player_two
+    else
+      @current_player = @player_one
+    end
+  end
+
+end
+
+tic_tac_toe = TicTacToe.new
+tic_tac_toe.play
